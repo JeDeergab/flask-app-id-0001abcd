76c386b5cd94acb3022c061347626306082871b5
John Enerio <JohnEnerio@gmail.com>
2024-10-07 14:50:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,24 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(
     __name__,
+    # do pwd on a directory to get the file path (ex: /home/johnenerio/)
     static_folder='/home/johnenerio/static',
     template_folder='/home/johnenerio/templates'
 )
 
+# When JavaScript is disabled it will fill-in the contents in index.html file
 @app.route('/')
 def home():
     noscript_content = "JavaScript is disabled or not supported by your browser."
     return render_template('index.html', noscript_content=noscript_content)
 
+# This is being fetch in the script file. 
 @app.route('/top_nav')
 def top_navigation_menu():
     topnav_html = '<div class="top_nav"></div>'
     return jsonify({'html': topnav_html})
-    
+
+# This is being fetch in the script file.    
 @app.route('/body')
 def body():
     body_html = '<div class="main_body"></div>'
